ecd09ca139fffc34d9c36e5df4cc58c16fd574c3
timueh <tillmann.muehlpfordt@kit.edu>
2022-05-12 16:01:51 +0200

    create from yaml

--- py_epc_qr/transaction.py
+++ py_epc_qr/transaction.py
@@ -1,44 +1,54 @@
 import qrcode
+import yaml
+
+ALLOWED_KEYS = [
+            'beneficiary',
+            'iban',
+            'amount',
+            'remittance'
+            ]
 
 rows = {
-    1: 'bcd',
-    2: 'version',
-    3: 'encoding',
-    4: 'identification_code',
-    5: 'bic',
-    6: 'beneficiary',
-    7: 'iban',
-    8: 'amount',
-    9: 'purpose',
-    10: 'remittance_structured',
-    11: 'remittance_unstructured',
-    12: 'originator_information'
+    1: "bcd",
+    2: "version",
+    3: "encoding",
+    4: "identification_code",
+    5: "bic",
+    6: "beneficiary",
+    7: "iban",
+    8: "amount",
+    9: "purpose",
+    10: "remittance_structured",
+    11: "remittance_unstructured",
+    12: "originator_information",
 }
 
 ENCODINGS = {
-    1: 'UTF-8',
-    2: 'ISO-8859-1',
-    3: 'ISO-8859-2',
-    4: 'ISO-8859-4',
-    5: 'ISO-8859-5',
-    6: 'ISO-8859-7',
-    7: 'ISO-8859-10',
-    8: 'ISO-8859-15',
+    1: "UTF-8",
+    2: "ISO-8859-1",
+    3: "ISO-8859-2",
+    4: "ISO-8859-4",
+    5: "ISO-8859-5",
+    6: "ISO-8859-7",
+    7: "ISO-8859-10",
+    8: "ISO-8859-15",
 }
+
 
 class epc_qr:
     def __init__(
-                self,
-                version: str,
-                encoding: int,
-                bic: str,
-                beneficiary: str,
-                iban: str,
-                amount: float,
-                purpose: str,
-                remittance_structured: str,
-                remittance_unstructured: str,
-                originator_information: str):
+        self,
+        version: str,
+        encoding: int,
+        bic: str,
+        beneficiary: str,
+        iban: str,
+        amount: float,
+        purpose: str,
+        remittance_structured: str,
+        remittance_unstructured: str,
+        originator_information: str,
+    ):
         self.bcd = "BCD"
         self.bic = bic
         self.version = version
@@ -52,22 +62,22 @@ class epc_qr:
         self.remittance_unstructured = remittance_unstructured
         self.originator_information = originator_information
 
-    def to_txt(self, file_name: str = 'qr_source.txt'):
-        with open(file_name, 'w', encoding=self.resolve_encoding()) as file:
+    def to_txt(self, file_name: str = "qr_source.txt"):
+        with open(file_name, "w", encoding=self.resolve_encoding()) as file:
             for key, value in rows.items():
                 file.write(self.__getattribute__(value))
                 if key < 12:
-                    file.write('\n')
+                    file.write("\n")
 
     def to_str(self) -> str:
-        res = ''
+        res = ""
         for key, value in rows.items():
             res += self.__getattribute__(value)
             if key < 12:
-                res += '\n'
+                res += "\n"
         return res
 
-    def to_qr(self, file_name: str = 'qr.png'):
+    def to_qr(self, file_name: str = "qr.png"):
         qr = qrcode.QRCode(
             version=6,
             error_correction=qrcode.constants.ERROR_CORRECT_M,
@@ -76,7 +86,10 @@ class epc_qr:
         qr.make()
         img = qr.make_image()
         img.save(file_name)
-        print(f'created image')
+        print("created image")
+        
+
+    # Properties
 
     @property
     def version(self):
@@ -84,10 +97,10 @@ class epc_qr:
 
     @version.setter
     def version(self, value: str):
-        if value not in (valid_versions:=['001', '002']):
-            raise ValueError(f'invalid version `{value}` (choose from {valid_versions}')
-        if value == '001' and not self.bic:
-            raise AssertionError('version 001 requires a BIC')
+        if value not in (valid_versions := ["001", "002"]):
+            raise ValueError(f"invalid version `{value}` (choose from {valid_versions}")
+        if value == "001" and not self.bic:
+            raise AssertionError("version 001 requires a BIC")
         self.__version = value
 
     @property
@@ -98,11 +111,11 @@ class epc_qr:
     def amount(self, value):
         value = float(value)
         if not 0.01 <= value <= 999999999.99:
-            raise ValueError(f'the amount {value} is out of bounds')
+            raise ValueError(f"the amount {value} is out of bounds")
 
         if value != round(value, 2):
-            raise ValueError(f'the amount is not a two-digit decimal number')
-        self.__amount = 'EUR{:.2f}'.format(value)
+            raise ValueError("the amount is not a two-digit decimal number")
+        self.__amount = "EUR{:.2f}".format(value)
 
     @property
     def encoding(self):
@@ -111,7 +124,7 @@ class epc_qr:
     @encoding.setter
     def encoding(self, value):
         if not 1 <= int(value) <= 8:
-            raise ValueError('encoding must be between 1 and 8')
+            raise ValueError("encoding must be between 1 and 8")
 
         self.__encoding = str(value)
 
@@ -125,28 +138,28 @@ class epc_qr:
     @beneficiary.setter
     def beneficiary(self, value: str):
         if not value.replace(" ", "").isalnum():
-            raise ValueError('beneficiary is not alphanumeric')
-        if len(value) > (max_length:=70):
-            raise ValueError(f'beneficiary must nood exceed {max_length} characters')
+            raise ValueError("beneficiary is not alphanumeric")
+        if not 1 <= len(value) <= (max_length := 70):
+            raise ValueError(f"beneficiary is mandatory, and must not exceed {max_length} characters")
         self.__beneficiary = value
 
     @property
     def iban(self):
         return self.__iban
-    
+
     @iban.setter
-    def iban(self, value):
+    def iban(self, value: str):
         if not value.isalnum():
-            raise ValueError('iban is not alphanumeric')
+            raise ValueError("iban is not alphanumeric")
         country_code = value[0:1]
         check_digits = value[2:3]
         bban = value[4:]
         if not country_code.isalpha():
-            raise ValueError('invalid iban country code')
+            raise ValueError("invalid iban country code")
         if not check_digits.isnumeric():
-            raise ValueError('invalid check digits')
+            raise ValueError("invalid check digits")
         if len(bban) > 30:
-            raise ValueError('bban is too long')
+            raise ValueError("bban is too long")
         self.__iban = value
 
     @property
@@ -156,33 +169,39 @@ class epc_qr:
     @remittance_unstructured.setter
     def remittance_unstructured(self, value):
         if not value.replace(" ", "").isalnum():
-            raise ValueError('unstructered remittance is non alphanumeric')
+            raise ValueError("unstructered remittance is non alphanumeric")
         if len(value) > 140:
-            raise ValueError('unstructured remittance exceeds 140 characters')
+            raise ValueError("unstructured remittance exceeds 140 characters")
         self.__remittance_unstructured = value
 
 
 class consumer_epc_qr(epc_qr):
-    def __init__(self, beneficiary: str,
-                 iban: str,
-                 amount: float,
-                 remittance: str):
+    def __init__(self, beneficiary: str, iban: str, amount: float, remittance: str):
         super().__init__(
-            version='002',
+            version="002",
             encoding=1,
-            bic='',
+            bic="",
             beneficiary=beneficiary,
             iban=iban,
             amount=amount,
-            purpose='',
-            remittance_structured='',
+            purpose="",
+            remittance_structured="",
             remittance_unstructured=remittance,
-            originator_information='',
+            originator_information="",
         )
+
+    @classmethod
+    def from_yaml(cls, file_name: str):
+        with open(file_name, 'r') as file:
+            data = yaml.safe_load(file)
+        if not sorted(list(data.keys())) == sorted(ALLOWED_KEYS):
+            raise AssertionError(f'yaml template has incorrect entries (allowed are {ALLOWED_KEYS})')
+        return cls(**data)
 
 
 if __name__ == "__main__":
-    epc = consumer_epc_qr('rolf beneficiary', 'DE18200411550436942700', 12.34, 'my reference')
-    epc.to_txt()
+    epc = consumer_epc_qr.from_yaml('template.yaml')
+    epc = consumer_epc_qr(
+        "Wikimedia Fördergesellschaft", "DE33100205000001194700", 1, "Danke"
+    )
     epc.to_qr()
-    breakpoint()
--- tests/test_py_epc_qr.py
+++ tests/test_py_epc_qr.py
@@ -2,6 +2,10 @@ from py_epc_qr import __version__
 from py_epc_qr.transaction import consumer_epc_qr, epc_qr
 
 import pytest
+import filecmp
+from os import remove
+import qrcode
+import PIL
 
 
 def test_version():
@@ -11,6 +15,66 @@ def get_valid_dummy_iban():
     return 'DE' + '1'*18
 
 class TestEpcQr:
+
+    @pytest.mark.parametrize('fun, given, expected_txt, expected_png', [
+        (
+            consumer_epc_qr,
+            {
+                'beneficiary': 'Wikimedia Foerdergesellschaft',
+                'iban': 'DE33100205000001194700',
+                'amount': 123.45,
+                'remittance': 'Spende fuer Wikipedia',
+            },
+            'tests/data/qr_version_002.txt',
+            'tests/data/qr_version_002.png',
+        ),
+        (
+            epc_qr,
+            {
+                'version': '001',
+                'encoding': 1,
+                'bic': 'BFSWDE33BER',
+                'beneficiary': 'Wikimedia Foerdergesellschaft',
+                'iban': 'DE33100205000001194700',
+                'amount': 123.45,
+                'purpose': '',
+                'remittance_structured': '',
+                'remittance_unstructured': 'Spende fuer Wikipedia',
+                'originator_information': '',
+            },
+            'tests/data/qr_version_001.txt',
+            'tests/data/qr_version_001.png',
+        ),
+    ])
+    def test_epc_qr_version(self, fun, given, expected_txt, expected_png):
+        """
+        Given the QR example from wikipedia
+        When creating an epc_qr as txt and png
+        Then everything works as intended
+        """
+        qr = fun(**given)
+        qr.to_txt(tmp_txt:='out.txt')
+        qr.to_qr(tmp_png:='out.png')
+        assert filecmp.cmp(tmp_txt, expected_txt)
+        assert filecmp.cmp(tmp_png, expected_png)
+        remove(tmp_txt)
+        remove(tmp_png)
+
+    @pytest.mark.parametrize('value', [0, 9])
+    def test_epc_qr_encoding_raises_exception(self, value):
+        with pytest.raises(ValueError):
+            epc_qr(
+                version='002',
+                encoding=value,
+                bic='',
+                beneficiary='ben benefit',
+                iban=get_valid_dummy_iban(),
+                amount=10,
+                purpose='',
+                remittance_structured='',
+                remittance_unstructured='',
+                originator_information='',
+            )
 
     @pytest.mark.parametrize('value', [
         0,
@@ -33,7 +97,7 @@ class TestEpcQr:
         with pytest.raises(ValueError):
             consumer_epc_qr('me', get_valid_dummy_iban(), value, '')
 
-    @pytest.mark.parametrize('value', ['a'*71, '§23'])
+    @pytest.mark.parametrize('value', ['', 'a'*71, '§23'])
     def test_epc_qr_beneficiary_raises_exception(self, value):
         """
         Given an beneficiary that is too long
@@ -62,7 +126,7 @@ class TestEpcQr:
         with pytest.raises(AssertionError):
             epc_qr('001', 1, '', '', get_valid_dummy_iban(), 10.00, '', '', '', '')
 
-    @pytest.mark.parametrize('value', ['123', 'DEA1', 'DE12%', 'DE12' + 'a' * 31])
+    @pytest.mark.parametrize('value', ['123', 'DEA1', 'DE12%', 'DE12' + 'a' * 31, 'Hallo ,:'])
     def test_epc_qr_iban_invalid_raises_exception(self, value):
         """
         Given an iban with an invalid country code
@@ -70,7 +134,7 @@ class TestEpcQr:
         Then an exception is raised
         """
         with pytest.raises(ValueError):
-            consumer_epc_qr('',value,0.01,'')
+            consumer_epc_qr('ben benefit',value,0.01,'')
 
     @pytest.mark.parametrize('value', ['DE12%', 'a' * 141, '%' * 141])
     def test_epc_qr_unstructured_remittance_raises_exception(self, value):
@@ -80,4 +144,19 @@ class TestEpcQr:
         Then an exception is raised
         """
         with pytest.raises(ValueError):
-            consumer_epc_qr('',get_valid_dummy_iban(),0.01,value)
+            consumer_epc_qr('ben benefit',get_valid_dummy_iban(),0.01,value)
+
+    def test_epc_qr_from_yaml(self):
+        epc_qr_src = consumer_epc_qr.from_yaml('tests/template_version_002.yaml')
+        epc_qe_tgt = consumer_epc_qr(
+            beneficiary = 'Wikimedia Foerdergesellschaft',
+            iban = 'DE33100205000001194700',
+            amount = 10,
+            remittance = 'Danke',
+        )
+        assert epc_qr_src.to_txt() == epc_qe_tgt.to_txt()
+    
+    def test_epc_qr_from_yaml_raises_exception(self):
+        with pytest.raises(AssertionError):
+            consumer_epc_qr.from_yaml('tests/template_version_002_broken.yaml')
+        
